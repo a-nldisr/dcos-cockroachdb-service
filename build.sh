594b2664055c3cda887866a04c526ca22a174409
a-nldisr <1867776+a-nldisr@users.noreply.github.com>
2019-10-02 15:32:40 +0200

    solved build errors with build framework script from dcos commons

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ FRAMEWORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$(dirname "$(dirname $FRAMEWORK_DIR)")"
 export TOOLS_DIR=${ROOT_DIR}/tools
 BUILD_DIR=$FRAMEWORK_DIR/build/distributions
-${ROOT_DIR}/tools/build_framework.sh cockroachdb $FRAMEWORK_DIR --artifact "$BUILD_DIR/executor.zip" --artifact "$BUILD_DIR/cockroachdb-scheduler.zip" $@
+${ROOT_DIR}/tools/build_package.sh cockroachdb $FRAMEWORK_DIR -a "$BUILD_DIR/executor.zip" -a "$BUILD_DIR/cockroachdb-scheduler.zip" $@
